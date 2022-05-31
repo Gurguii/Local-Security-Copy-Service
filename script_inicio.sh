21758865a7b2b056489fc743d8bf21658600b7ed
Airán Gómez <101645735+Gurguii@users.noreply.github.com>
2022-05-31 13:37:35 +0100

    Update script_inicio.sh

--- script_inicio.sh
+++ script_inicio.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+max=maxLogs
+contador=0
 while true; do
-  echo $(date) >> rutaLogs
-  rsync -zvhr copiar pegar >> rutaLogs
-  sleep tiempo
+  if (( $contador == $max )); then
+    ultimoFichero=$(ls -lt rutaLogs | tail -1 | awk '{print $9}')
+    rm -f rutaLogs/$ultimoFichero
+    rsync -zvhr copiar pegar >> rutaLogs/"$(date | tr " " "_")"
+    sleep tiempo
+  else
+    rsync -zvhr copiar pegar >> rutaLogs/"$(date | tr " " "_")"
+    let contador+=1
+    sleep tiempo
+  fi
 done
