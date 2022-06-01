5e6a4bc7071efe6cc28cc9e173263177437ede98
Airán Gómez <101645735+Gurguii@users.noreply.github.com>
2022-06-01 22:47:24 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,7 +19,7 @@ while true; do
 done
 read -p "Ruta a copiar: " rutaCopia
 if [[ ! -e $rutaCopia || ! -d $rutaCopia ]]; then
-  printf "La ruta %s no existe " $rutaCopia
+  printf "La ruta %s no existe" $rutaCopia
   read -p "¿Crear? s/n: " rsp
   if [[ ${rsp,,} == "s" || ${rsp,,} == "si" ]]; then
     mkdir -p $rutaCopia && printf "[!] Ruta creada\n"
@@ -30,7 +30,7 @@ if [[ ! -e $rutaCopia || ! -d $rutaCopia ]]; then
 fi
 read -p "Ruta de copia: " rutaBackup
 if [[ ! -e $rutaBackup || ! -d $rutaBackup ]]; then
-  printf "La ruta %s no existe " $rutaCopia
+  printf "La ruta %s no existe" $rutaCopia
   read -p "¿Crear? s/n: " rsp
   if [[ ${rsp,,} == "s" || ${rsp,,} == "si" ]]; then
     mkdir -p $rutaBackup && printf "[!] Ruta creada\n"
@@ -39,16 +39,8 @@ if [[ ! -e $rutaBackup || ! -d $rutaBackup ]]; then
     exit 0
   fi
 fi
-read -p "Ruta para guardar los scripts: " rutaScripts
-if [[ ! -e $rutaScripts || ! -d $rutaScripts ]]; then
-  printf "La ruta %s no existe " $rutaScripts
-  read -p "¿Crear? s/n: " rsp
-  if [[ ${rsp,,} == "s" || ${rsp,,} == "si" ]]; then
-    mkdir -p $rutaScripts && printf "[!] Ruta creada\n"
-  else
-    printf "Saliendo..."
-    exit 0
-  fi
+if [[ ! -e scripts_servicios || ! -d scripts_servicios ]]; then
+  mkdir $(pwd)/scripts_servicios
 fi
 while true; do
   read -p "Tiempo entre copias(minutos): " tiempo
@@ -69,9 +61,6 @@ while true; do
   fi
 done
 
-if [[ ${rutaScripts:(-1)} == "/" ]]; then
-  rutaScripts=${rutaScripts:0:$((${#rutaScripts}-1))}
-fi
 if [[ ${rutaCopia:(-1)} == "/" ]]; then
   rutaCopia=${rutaCopia:0:$((${#rutaCopia}-1))}
 fi
@@ -95,15 +84,15 @@ else
 fi
 
 #CREAR SCRIPT INICIO
-cat script_inicio.sh | sed "/copiar/s||$rutaCopia|" | sed "/pegar/s||$rutaBackup/|" | sed "/tiempo/s||$(($tiempo*60))|" | sed "/rutaLogs/s||$(pwd)/logs_Servicios/servicio_$nombreServicio|" | sed "0,/maxLogs/s||$maxLogs|" >> $rutaScripts/inicio_servicio_$nombreServicio.sh
-sudo chmod u+x $rutaScripts/inicio_servicio_$nombreServicio.sh
+cat script_inicio.sh | sed "/copiar/s||$rutaCopia|" | sed "/pegar/s||$rutaBackup/|" | sed "/tiempo/s||$(($tiempo*60))|" | sed "/rutaLogs/s||$(pwd)/logs_Servicios/servicio_$nombreServicio|" | sed "0,/maxLogs/s||$maxLogs|" >> $(pwd)/scripts_servicios/script_$nombreServicio.sh
+sudo chmod u+x $(pwd)/scripts_servicios/script_$nombreServicio.sh
 
 #CREAR SCRIPT ELIMINAR SERVICIO
-cat eliminar_servicio.sh | sed "0,/scripts/s||$rutaScripts/inicio_servicio_$nombreServicio.sh|" | sed "0,/nombre/s||$nombreServicio|" | sed "0,/logs/s||$(pwd)/logs_Servicios/servicio_$nombreServicio|" >> $(pwd)/eliminar_Servicios/eliminar_$nombreServicio.sh
+cat eliminar_servicio.sh | sed "0,/scripts/s||$(pwd)/scripts_servicios/script_$nombreServicio.sh|" | sed "0,/nombre/s||$nombreServicio|" | sed "0,/logs/s||$(pwd)/logs_Servicios/servicio_$nombreServicio|" >> $(pwd)/eliminar_Servicios/eliminar_$nombreServicio.sh
 echo "[!] Scripts de inicio y fin creados"
 
 #CREAR SERVICIO
-cat servicio.txt | sed "0,/inicioScript/s||$rutaScripts/inicio_servicio_$nombreServicio.sh|" | sed "0,/finScript/s||$rutaScripts/fin_servicio_$nombreServicio|" >> /etc/systemd/system/$nombreServicio.service
+cat servicio.txt | sed "0,/inicioScript/s||$(pwd)/scripts_servicios/script_$nombreServicio.sh|" >> /etc/systemd/system/$nombreServicio.service
 echo "[!] Servicio creado"
 sudo systemctl daemon-reload
 
